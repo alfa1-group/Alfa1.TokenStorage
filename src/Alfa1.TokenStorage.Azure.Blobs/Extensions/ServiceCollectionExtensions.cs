dcdd226a60639a939a2e5a3103f2c1c2719d2041
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2026-05-12 09:40:43 +0200

    Add initial code (#1)

---
+++ src/Alfa1.TokenStorage.Azure.Blobs/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Alfa1.TokenStorage.Abstractions;
+using Azure.Storage.Blobs;
+using Alfa1.TokenStorage.Azure.Blobs;
+using Alfa1.TokenStorage.Azure.Blobs.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTokenStorageAzureBlobs(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<AzureBlobsTokenStorageOptions>()
+            .Bind(configuration.GetSection(nameof(AzureBlobsTokenStorageOptions)))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddMemoryCache();
+
+        services.AddSingleton(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<AzureBlobsTokenStorageOptions>>();
+            return new BlobContainerClient(options.Value.ConnectionString, options.Value.ContainerName);
+        });
+
+        return services.AddSingleton<ITokenStorageService, AzureBlobsTokenStorageService>();
+    }
+}
---
+++ src/Alfa1.TokenStorage.SqlServer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Alfa1.TokenStorage.Abstractions;
+using Alfa1.TokenStorage.SqlServer;
+using Alfa1.TokenStorage.SqlServer.Data;
+using Alfa1.TokenStorage.SqlServer.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTokenStorageSqlServer(this IServiceCollection services, IConfiguration configuration, ServiceLifetime dbContextLifetime = ServiceLifetime.Scoped)
+    {
+        services.AddOptions<EntityFrameworkCoreTokenStorageOptions>()
+            .Bind(configuration.GetSection("SqlServerTokenStorageOptions"))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddMemoryCache();
+
+        services.AddDbContext<AuthenticationTokenDbContext>((serviceProvider, options) =>
+        {
+            var connectionString = configuration.GetConnectionString(serviceProvider.GetOptions().ConnectionStringName);
+
+            options.UseSqlServer(connectionString);
+        }, dbContextLifetime);
+
+        services.EnsureTokenTableExists();
+
+        return dbContextLifetime switch
+        {
+            ServiceLifetime.Singleton => services.AddSingleton<ITokenStorageService, EntityFrameworkCoreTokenStorageService>(),
+            ServiceLifetime.Scoped => services.AddScoped<ITokenStorageService, EntityFrameworkCoreTokenStorageService>(),
+            _ => services.AddTransient<ITokenStorageService, EntityFrameworkCoreTokenStorageService>()
+        };
+    }
+
+    private static EntityFrameworkCoreTokenStorageOptions GetOptions(this IServiceProvider serviceProvider)
+    {
+        return serviceProvider.GetRequiredService<IOptions<EntityFrameworkCoreTokenStorageOptions>>().Value;
+    }
+
+    private static void EnsureTokenTableExists(this IServiceCollection services)
+    {
+        using var serviceProvider = services.BuildServiceProvider();
+        using var scope = serviceProvider.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<AuthenticationTokenDbContext>();
+
+        dbContext.Database.EnsureCreated();
+        dbContext.EnsureAuthenticationTokenTableExists();
+    }
+}
